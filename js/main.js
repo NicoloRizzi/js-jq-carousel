b67a950e2e5f3f8f10fa1853b59261bd1db7a63e
Nicolo <nicolorizzi1996@gmail.com>
2020-04-09 17:10:03 +0200

    BASE ELEMENT

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
+/**
+ * CREARE UNO SLIDER DI IMMAGINI
+ */
+
+/**
+  * PSEUDO CODICE:
+  * 
+*/
 $(document).ready ( function (){
-
-
-
-
-
-
-
-
-
-
-
-
-
   
+  // REFERENZA
+
+  var btnPrevious = $('.fa-arrow-left');
+  var btnNext = $('.fa-arrow-right');
+
+
+
+btnNext.click( function (){
+
+});
+
+
+
+
+
+
+
+
+// FINE DOCUMENT READY
 });
