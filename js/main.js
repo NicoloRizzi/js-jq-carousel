99c22538c8c707321fce81d3810a5c90389b2e02
Nicolo <nicolorizzi1996@gmail.com>
2020-04-10 12:51:53 +0200

    CREAZIONE PRIMI EVENTI CLICK

--- js/main.js
+++ js/main.js
@@ -4,20 +4,34 @@
 
 /**
   * PSEUDO CODICE:
+  * 1) TARHETTIZZO I BOTTONI
+  * 2) TARGHETTIZZO LA PRIMA IMMAGINE
+  * 3) TARCHETTIZZO L'ULTIMA IMMAGINE
   * 
 */
+
 $(document).ready ( function (){
+
+  var btnPrevious = $('.block-sx > i');
+  var btnNext = $('.block-dx > i');
+  var imageFirst = $('.first');
+  var imageLast = $('.last');
+
+
+  btnNext.click ( function (){
+    // CREO LA MIA VARIABILE CHE CONTERRA' L'ELEMENTO CORRENTE
+    var imageCurrent = $('.container-img .active');
+    imageCurrent.removeClass('active');
+    imageCurrent.next().addClass('active');
+  });
+
+  btnPrevious.click ( function (){
+    var imageCurrent = $('.container-img .active');
+    imageCurrent.removeClass('active');
+    imageCurrent.prev().addClass('active');
+  });
+
   
-  // REFERENZA
-
-  var btnPrevious = $('.fa-arrow-left');
-  var btnNext = $('.fa-arrow-right');
-
-
-
-btnNext.click( function (){
-
-});
 
 
 
